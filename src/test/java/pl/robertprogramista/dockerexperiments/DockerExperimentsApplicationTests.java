a95fca581187e1f353cc80b8cde7294ee7c747be
userwebsky <33402904+userwebsky@users.noreply.github.com>
2023-01-23 07:48:40 +0100

    init

---
+++ src/test/java/pl/robertprogramista/dockerexperiments/DockerExperimentsApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.robertprogramista.dockerexperiments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerExperimentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
